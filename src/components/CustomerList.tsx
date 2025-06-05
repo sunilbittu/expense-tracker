a136237778a3739eaf5df0c727e9f22e37c5a3df
sunilbittu <pavanksunil@gmail.com>
2025-06-05 13:24:35 +0530

    Deploy application to Netlify

--- src/components/CustomerList.tsx
+++ src/components/CustomerList.tsx
@@ -92,9 +92,9 @@ const CustomerList: React.FC<CustomerListProps> = ({ onEditCustomer, onAddCustom
   };
   
   const formatCurrency = (amount: number) => {
-    return new Intl.NumberFormat('en-US', {
+    return new Intl.NumberFormat('en-IN', {
       style: 'currency',
-      currency: 'USD',
+      currency: 'INR',
     }).format(amount);
   };
   
